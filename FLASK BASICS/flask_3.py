9f03f1b6f34a15c17d11c9d7a28b851a3b36a7c4
ganigithub <86476881+ganigithub@users.noreply.github.com>
2022-03-25 13:03:47 +0530

    Add files via upload

---
+++ FLASK BASICS/flask_3.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return "<h1>This about us page. We are a tech campany<h1>"
+
+if __name__=='__main__':
+    app.run(debug=True)
